edc40699fdc4ac764741d47a1cc095bc7908ea1b
MintMissy <dawidkostka480@gmail.com>
2021-11-15 23:30:05 +0100

    Add basic navbar structure, start working at adding modular scss

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Navbar.module.scss';
+import Logo from '../../assets/logo.png';
+import { FaWindowClose, FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div classes={classes['navbar']}>
+      <div className='navbar-header'>
+        <div className={classes['navbar-header__logo']}>
+          <img src={Logo} alt='Logo' />
+        </div>
+        {toggleMenu ? <FaWindowClose /> : <FaBars />}
+      </div>
+
+      <ul className={classes['navbar-links']}>
+        <li className={classes['navbar__link']}>
+          <a href='#departments'>departments</a>
+        </li>
+        <li className={classes['navbar__link']}>
+          <a href='#achievements'>achievements</a>
+        </li>
+        <li className={classes['navbar__link']}>
+          <a href='#news'>news</a>
+        </li>
+        <li className={classes['navbar__link']}>JOIN</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
