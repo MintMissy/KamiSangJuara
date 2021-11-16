2036e1370faf36fce4791febfa25b60724e389a4
MintMissy <dawidkostka480@gmail.com>
2021-11-16 23:11:15 +0100

    Create responsive navbar -> write styles and code react actions

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,62 @@
 import React, { useState } from 'react';
-import classes from './Navbar.module.scss';
+import styles from './Navbar.module.scss';
 import Logo from '../../assets/logo.png';
 import { FaWindowClose, FaBars } from 'react-icons/fa';
+
+function Menu({ onLinkClick }) {
+  return (
+    <>
+      <li className={styles['navbar__link']} onClick={() => onLinkClick()}>
+        <a href='#departments'>departments</a>
+      </li>
+      <li className={styles['navbar__link']} onClick={() => onLinkClick()}>
+        <a href='#achievements'>achievements</a>
+      </li>
+      <li className={styles['navbar__link']} onClick={() => onLinkClick()}>
+        <a href='#news'>news</a>
+      </li>
+      <li className={styles['navbar__link']} onClick={() => onLinkClick()}>
+        join
+      </li>
+    </>
+  );
+}
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div classes={classes['navbar']}>
-      <div className='navbar-header'>
-        <div className={classes['navbar-header__logo']}>
+    <div className={styles.navbar}>
+      <div className={styles['navbar-header']}>
+        <div className={styles['navbar-header__logo']}>
           <img src={Logo} alt='Logo' />
         </div>
-        {toggleMenu ? <FaWindowClose /> : <FaBars />}
+        {toggleMenu ? (
+          <FaWindowClose
+            className={styles['navbar-header__toggle-icon']}
+            onClick={() => {
+              setToggleMenu(false);
+            }}
+          />
+        ) : (
+          <FaBars
+            className={styles['navbar-header__toggle-icon']}
+            onClick={() => {
+              setToggleMenu(true);
+            }}
+          />
+        )}
       </div>
 
-      <ul className={classes['navbar-links']}>
-        <li className={classes['navbar__link']}>
-          <a href='#departments'>departments</a>
-        </li>
-        <li className={classes['navbar__link']}>
-          <a href='#achievements'>achievements</a>
-        </li>
-        <li className={classes['navbar__link']}>
-          <a href='#news'>news</a>
-        </li>
-        <li className={classes['navbar__link']}>JOIN</li>
+      <ul className={styles['navbar-links']}>
+        {<Menu onLinkClick={() => {}} />}
       </ul>
+
+      {toggleMenu && (
+        <ul className={styles['navbar-mobile-links']}>
+          <Menu onLinkClick={() => setToggleMenu(false)} />
+        </ul>
+      )}
     </div>
   );
 };
