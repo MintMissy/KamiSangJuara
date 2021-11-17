53076a7bd03c6a1e0b31b7261eb4dd374b468e1f
MintMissy <dawidkostka480@gmail.com>
2021-11-17 22:41:00 +0100

    Finish home hero

--- src/containers/home-hero/HomeHero.jsx
+++ src/containers/home-hero/HomeHero.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import classes from './HomeHero.module.scss';
+import styles from './HomeHero.module.scss';
 
 const HomeHero = () => {
-  return <div>HomeHero</div>;
+  return (
+    <div className={styles['home-hero']}>
+      <div className={styles['home-hero-content']}>
+        <h1>KAMI SANG JUARA</h1>
+        <p>Sekolah Kejuruan terbaik di Indonesia Timur</p>
+      </div>
+
+      <button className={styles['home-hero-button']}>INFORMASI PPDB</button>
+    </div>
+  );
 };
 
 export default HomeHero;
