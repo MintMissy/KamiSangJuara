1529f11b7b71b0912c49f91b226882b501d87b2f
MintMissy <dawidkostka480@gmail.com>
2021-11-20 23:27:16 +0100

    Add location section

--- src/containers/location/Location.jsx
+++ src/containers/location/Location.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import classes from './Location.module.scss';
+import styles from './Location.module.scss';
+import Logo from '../../assets/logo.png';
+import LocationImg from '../../assets/location.png';
 
 const Location = () => {
-  return <div>Location</div>;
+  return (
+    <div className={styles['location']}>
+      <div className={styles['location-content']}>
+        <div className={styles['location-content__logo']}>
+          <img src={Logo} alt='Logo' />
+        </div>
+        <p>
+          Dengan tekad hendak membentuk insan berkarakter unggul dalam kerangka
+          pencerdasan anak bangsa dan pembangunan peradaban bangsa
+        </p>
+        <address>
+          Alamat : Jl. A.P Pettarani No. Gn Sari, Rappocini, Kota Makassar,
+          Sulawesi Selatan 90222
+        </address>
+      </div>
+      <div className={styles['location-image']}>
+        <img src={LocationImg} alt='' />
+      </div>
+    </div>
+  );
 };
 
 export default Location;
