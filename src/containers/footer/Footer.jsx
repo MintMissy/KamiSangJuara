d02febc94c6c704bb757dbe71b531627d33f5669
MintMissy <dawidkostka480@gmail.com>
2021-11-20 23:36:37 +0100

    Add footer section

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
-import classes from './Footer.module.scss';
+import styles from './Footer.module.scss';
+import FacebookIcon from '../../assets/icon-fb.png';
+import InstagramIcon from '../../assets/icon-instagram.png';
+import TwitterIcon from '../../assets/icon-twitter.png';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles['footer']}>
+      <div className={styles['footer-content']}>
+        © Copyright 2019 | SMK Telkom Makassar | Powered by Carakde.id
+      </div>
+      <div className={styles['footer-socials']}>
+        <img src={FacebookIcon} alt='Facebook' />
+        <img src={InstagramIcon} alt='Instagram' />
+        <img src={TwitterIcon} alt='Twitter' />
+      </div>
+    </div>
+  );
 };
 export default Footer;
